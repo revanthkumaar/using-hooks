5083f652b979a00a868da7a1b0f226329b01c175
revanthkumaar <iamrevanthkumar@gmail.com>
2021-05-07 18:12:15 +0530

    dlasf
    qlakjsd

---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from "redux";
+
+import asyncReducer from "./reducers";
+
+
+import thunk from "redux-thunk";
+
+ const store = createStore(asyncReducer, applyMiddleware(thunk))
+
+
+export default store;
+
